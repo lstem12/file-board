f399b2eb629c31127259a4e38d5e88beb8e8feb9
Host <Host@220.116.195.193>
2020-09-16 17:11:48 +0900

    1

--- src/com/file/board/service/impl/PhotoBoardServiceImpl.java
+++ src/com/file/board/service/impl/PhotoBoardServiceImpl.java
@@ -1,12 +1,14 @@
 package com.file.board.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.file.board.dao.PhotoBoardDAO;
 import com.file.board.service.PhotoBoardService;
 import com.file.board.vo.PhotoBoardVO;
 
+@Service
 public class PhotoBoardServiceImpl implements PhotoBoardService {
 
 	@Autowired
